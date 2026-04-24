3abdaf1851584fc024eeb1789a9f3c9fb823e440
nuraskhan <220107085@stu.sdu.edu.kz>
2026-04-24 23:40:49 +0500

    Fix sessions.token column to TEXT, push to origin

--- library/src/main/java/sdu/edu/kz/diploma/library/model/entity/Session.java
+++ library/src/main/java/sdu/edu/kz/diploma/library/model/entity/Session.java
@@ -18,7 +18,7 @@ public class Session {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false, unique = true)
+    @Column(nullable = false, unique = true, columnDefinition = "TEXT")
     private String token;
 
     @ManyToOne(fetch = FetchType.LAZY)
