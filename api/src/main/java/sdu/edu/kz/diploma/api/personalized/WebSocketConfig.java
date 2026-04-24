51873a7bf605811d7aee9a45b5102064f9f3534e
nuraskhan <220107085@stu.sdu.edu.kz>
2026-04-25 01:01:16 +0500

    Allow Heroku frontend origin in WebSocket CORS config

--- api/src/main/java/sdu/edu/kz/diploma/api/personalized/WebSocketConfig.java
+++ api/src/main/java/sdu/edu/kz/diploma/api/personalized/WebSocketConfig.java
@@ -20,7 +20,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws/chat")
-                .setAllowedOriginPatterns("http://localhost:5173")
+                .setAllowedOriginPatterns("http://localhost:5173", "https://diploma-ui-881ce33bc920.herokuapp.com")
                 .withSockJS();
     }
 }
